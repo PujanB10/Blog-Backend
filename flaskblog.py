7679cdbbffb0b62982f6303411b08a98e5f481a5
Pujan Bhattarai <bhattaraipoozan@gmail.com>
2023-02-13 18:50:21 +0545

    Initial commit

---
+++ flaskblog.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4649w4w894198fafav984894savfas'
+
+posts = [
+    {
+        "name":"Pujan Bhattarai",
+        "title":"My First Blog",
+        "date":"20 November, 2020"
+    },
+      {
+        "name":"Rajan Bhattarai",
+        "title":"My Second Blog",
+        "date":"14 December, 2020"
+    },
+      {
+        "name":"Sujan Bhattarai",
+        "title":"My Third Blog",
+        "date":"10 october, 2020"
+    }
+
+]
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title="Hello")
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
